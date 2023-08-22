8ea347e1e326922b8ba2cf2c509ca34ce3d2a782
HuangWei <market@zuiyue.com>
2023-08-22 19:05:25 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use serde_json::Value;
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    wei_env::bin_init("wei-transmission");
+    let args: Vec<String> = env::args().collect();
+
+    // if args.len() < 2 {
+    //     help();
+    //     std::process::exit(1);
+    // }
+    let command = &args[1];
+
+    match command.as_str() {
+        "install" => {
+            println!("Installing...");
+        },
+        "uninstall" => {
+            println!("Uninstalling...");
+        },
+        "check" => {
+
+        },
+        "api" => {
+            // api().await?;
+        },
+        _ | "help" => {
+            help();
+            std::process::exit(1);
+        }
+    }
+
+    Ok(())
+}
+
+fn help() {
+    let args: Vec<String> = env::args().collect();
+    println!("Usage:");
+    println!("  {} install", args[0]);
+    println!("  {} uninstall", args[0]);
+    println!("  {} api <report_url_process> <url> <json>", args[0]);
 }
