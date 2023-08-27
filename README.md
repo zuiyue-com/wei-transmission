548387d4c3d1027f3158d6b20a34c2d3e672f058
HuangWei <market@zuiyue.com>
2023-08-27 20:50:12 +0800

    init

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # wei-transmission
 
-- 开启 transmission-daemon
-- 设定存放地址
-- 设定 DHT,LPD,UPnP,uTP,peerport
-- 通过 rpc 下载文件
+- [ ] 开启 transmission-daemon
+- [ ] 设定存放地址
+- [ ] 设定 DHT,LPD,UPnP,uTP,peerport
+- [ ] 通过 rpc 下载文件
+- [ ] 重新打开软件继续下载
+- [ ] 提供查询功能
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,55 @@
 use std::env;
 use serde_json::Value;
 
+use reqwest::Client;
+use serde_json::json;
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     wei_env::bin_init("wei-transmission");
     let args: Vec<String> = env::args().collect();
 
-    // if args.len() < 2 {
-    //     help();
-    //     std::process::exit(1);
-    // }
     let command = &args[1];
 
     match command.as_str() {
-        "install" => {
-            println!("Installing...");
+        "add" => {
+            let client = Client::new();
+
+            let resp = client.post("http://localhost:9091/transmission/rpc")
+                .send().await?;
+
+            let session_id = &resp.headers()["X-Transmission-Session-Id"]; 
+
+            println!("{:#?}", session_id);
+
+
+            let resp = client.post("http://localhost:9091/transmission/rpc")
+                .header("X-Transmission-Session-Id", session_id) 
+                .json(&json!({
+                    "method": "torrent-add",
+                    "arguments": {
+                        "paused": false, 
+                        "filename": " magnet:?xt=urn:btih:759066CF9DBD7A473F2C5BB71BE9F77F05BF3191"
+                    }
+                }))
+                .send().await?
+                .text().await?;
+                //.json::<serde_json::Value>().await?;
+        
+            println!("{:#?}", resp);
+
+            let resp = client.post("http://localhost:9091/transmission/rpc")
+            .header("X-Transmission-Session-Id", session_id) 
+    .json(&json!({
+        "method": "torrent-start",
+        "arguments": {
+            "ids": "2"
+        }
+    }))
+    .send().await?
+    .text().await?;
+
+  println!("{:#?}", resp);
         },
         "uninstall" => {
             println!("Uninstalling...");
@@ -25,7 +60,7 @@ async fn main() -> Result<(), reqwest::Error> {
         "api" => {
             // api().await?;
         },
-        _ | "help" => {
+        _ => {
             help();
             std::process::exit(1);
         }
