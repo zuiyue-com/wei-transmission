3b27a88107b69551e18274b8dc0c898b9b7f27ca
HuangWei <market@zuiyue.com>
2023-08-28 16:07:33 +0800

    init

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # wei-transmission
 
-- 开启 transmission-daemon
+- 开启 transmission-daemon 
 - 设定存放地址
 - 设定 DHT,LPD,UPnP,uTP,peerport
-- 通过 rpc 下载文件
+- 通过 rpc 下载文件，接收到下载任务要
+- 解压文件
--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,11 @@ async fn main() -> Result<(), reqwest::Error> {
     wei_env::bin_init("wei-transmission");
     let args: Vec<String> = env::args().collect();
 
-    // if args.len() < 2 {
-    //     help();
-    //     std::process::exit(1);
-    // }
-    let command = &args[1];
+    let url = &args[1];
 
     match command.as_str() {
-        "install" => {
-            println!("Installing...");
-        },
-        "uninstall" => {
-            println!("Uninstalling...");
+        "url" => {
+            
         },
         "check" => {
 
@@ -25,7 +18,7 @@ async fn main() -> Result<(), reqwest::Error> {
         "api" => {
             // api().await?;
         },
-        _ | "help" => {
+        _  => {
             help();
             std::process::exit(1);
         }
@@ -37,7 +30,5 @@ async fn main() -> Result<(), reqwest::Error> {
 fn help() {
     let args: Vec<String> = env::args().collect();
     println!("Usage:");
-    println!("  {} install", args[0]);
-    println!("  {} uninstall", args[0]);
-    println!("  {} api <report_url_process> <url> <json>", args[0]);
+    println!("  {} <url>", args[0]);
 }
